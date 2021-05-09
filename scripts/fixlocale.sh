6d328418d11a7bf276976200e3360f4e929982d4
Peleg Daniel <41775219+pelegd@users.noreply.github.com>
2021-05-09 11:18:11 +0300

    Create fixlocale.sh

---
+++ scripts/fixlocale.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# source : Chris Titus
+# copied : 9/5/2021
+
+echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
+echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+echo "LANG=en_US.UTF-8" | sudo tee -a /etc/locale.conf
+sudo locale-gen en_US.UTF-8
