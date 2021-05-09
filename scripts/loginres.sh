302433b018bb2e8c471e6a441286d39a122ac40c
Peleg Daniel <41775219+pelegd@users.noreply.github.com>
2021-05-09 11:52:39 +0300

    Update loginres.sh

--- scripts/loginres.sh
+++ scripts/loginres.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# copied from chris titus
+# mode variable setup changed by peleg
+
 # This will setup the screen resolution on logon screen
 # In LightDM
 
-mode="$(xrandr -q|grep -A1 "VGA1 connected"| tail -1 |awk '{ print $1 }')"
+# mode="$(xrandr -q|grep -A1 "VGA1 connected"| tail -1 |awk '{ print $1 }')"
+mode="$(xrandr -q | grep "*" | awk '{ print $1 }')
 if [ -n "$mode" ]; then
   xrandr --output LVDS1 --off
   xrandr --output VGA1 --primary --mode 1920x1080
